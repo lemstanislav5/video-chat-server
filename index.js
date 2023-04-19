4555d2f7a558af9e6a7eabc6f1fdd5b625c68127
lemstanislav5 <lemstanislav5@gmail.com>
2023-04-19 18:38:37 +0300

    8217382730981723

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 const server = require('http').Server(app);
-app.get("/", (req, res) => {
-    res.status(200).send("Hello World");
+
+// app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
 });
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.param.room });
+});
+// app.get('/', (req, res) => {
+//   res.status(200).send('./public/index.html')
+// });
+
 server.listen(3030);
