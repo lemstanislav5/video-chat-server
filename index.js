566239e9fc2e068815ddaa506c8150f898372628
lemstanislav5 <lemstanislav5@gmail.com>
2023-04-20 09:27:11 +0300

    43532454325

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const { v4: uuidv4 } = require("uuid");
 const app = express();
 const server = require('http').Server(app);
 
-app.use(express.static('public'));
-app.get('/', (req, res) => res.redirect(`/room/${uuidv4()}/`));
+// app.use(express.static('public'));
+app.get('/', (req, res) => res.redirect(`/${uuidv4()}/`));
 let roomPath = '/room/:room([0-9a-f-]{36})';
-app.get(roomPath + '/', function(req, res) {
+app.get('/:room', function(req, res) {
+  console.log(req, res)
   res.sendFile(__dirname + '/public/index.html', {
     headers: {
       'Content-Security-Policy': [
